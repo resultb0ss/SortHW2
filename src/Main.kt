7d280ff8d895e503341b1f4e428086dc4e5b5755
Admin <reteamboss@gmail.com>
2024-09-15 12:57:23 +0300

    2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
-    println("Hello World!")
+    var array  = arrayOf(
+        arrayOf(1,2,3,45),
+        arrayOf(445,35,243,78),
+        arrayOf(564,345,24,65),
+    )
+
+    for (i in array){
+        println(i.sorted())
+    }
 }
